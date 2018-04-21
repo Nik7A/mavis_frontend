04c970dee1d2cd30df14631e2f6570cfe74c69b7
Nikolay Semernya <nsemernia@gmail.com>
2018-04-22 00:27:46 +0700

    React init

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # Mavis Frontend
-
-#### Stack:
-1. React + Redux + React Routing
-2. ES6 with Babel
-3. Yarn
-4. Webpack
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return <h1 className="tc">Home Page</h1>
+  }
+}
+
+export default App
